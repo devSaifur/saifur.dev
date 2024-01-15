d200b2712de0906528ddbb479a2e624b45bd2a5b
Saifur Rahman <saifur2akash@gmail.com>
2024-01-16 00:11:14 +0600

    almost done :)

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        primary:
+          "rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset",
+      },
+    },
   },
   plugins: [],
 };
