e088a738ccc441c24f492919785613b5098bda11
devSaifur <saifur2akash@gmail.com>
2024-05-18 23:40:25 +0600

    update font and iamge

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,12 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       fontFamily: {
-        primary: ['Inter', 'sans-serif'],
+        primary: ['Geist Sans', ...defaultTheme.fontFamily.sans],
       },
       boxShadow: {
         primary: 'inset_0_1px_0_0_rgba(148,163,184,0.1)',
