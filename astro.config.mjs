280f9a127d86019e6092ad11b5ecf3aef2ae5064
devSaifur <saifur2akash@gmail.com>
2024-06-01 20:16:18 +0600

    update icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-import icon from 'astro-icon'
 
 import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind()],
   output: 'server',
   adapter: cloudflare(),
 })
