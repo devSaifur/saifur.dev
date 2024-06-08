267f8594a00cc092e481baba567429532433726b
devSaifur <saifur2akash@gmail.com>
2024-06-09 00:06:17 +0600

    fix og image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig, sharpImageService } from 'astro/config'
 
 import tailwind from '@astrojs/tailwind'
 import cloudflare from '@astrojs/cloudflare'
@@ -7,9 +7,7 @@ import sitemap from '@astrojs/sitemap'
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
   output: 'server',
-  adapter: cloudflare({
-    imageService: 'cloudflare',
-  }),
+  adapter: cloudflare(),
   site: 'https://saifur.pages.dev',
   prefetch: {
     defaultStrategy: 'viewport',
