74f325a6149f42e38799d8009632ca95518afc8e
devSaifur <saifur2akash@gmail.com>
2024-06-08 22:58:37 +0600

    seo improvement

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 import { defineConfig } from 'astro/config'
+
 import tailwind from '@astrojs/tailwind'
-
 import cloudflare from '@astrojs/cloudflare'
+import sitemap from '@astrojs/sitemap'
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   output: 'server',
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    imageService: 'cloudflare',
+  }),
+  site: 'https://saifur.pages.dev',
+  prefetch: {
+    defaultStrategy: 'viewport',
+    prefetchAll: true,
+  },
 })
