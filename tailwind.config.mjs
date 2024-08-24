fd8345e9285dafca58e5ec6fac5f67fc1e44b2ec
devSaifur <saifur2akash@gmail.com>
2024-08-24 17:34:46 +0600

    some style fix

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,12 +12,9 @@ export default {
         primary: 'inset_0_1px_0_0_rgba(148,163,184,0.1)',
       },
       colors: {
-        foreground: 'rgb(var(--foreground) / <alpha-value>)',
-        primary: 'rgb(var(--primary) / <alpha-value>)',
         accent: {
           DEFAULT: 'rgb(var(--accent) / <alpha-value>)',
           purple: 'rgb(var(--accent-purple) / <alpha-value>)',
-          teal: 'rgb(var(--accent-teal) / <alpha-value>)',
         },
       },
     },
