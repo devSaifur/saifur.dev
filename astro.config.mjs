889125b69c7829024dab9dad46328a41ff1a19ed
devSaifur <saifur2akash@gmail.com>
2024-08-27 02:55:56 +0600

    remove react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import cloudflare from '@astrojs/cloudflare'
-import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), sitemap(), icon()],
-  output: 'hybrid',
+  integrations: [tailwind(), , sitemap(), icon()],
+  output: 'server',
   adapter: cloudflare(),
   site: 'https://saifur.pages.dev',
 })
