aacb6898eec72e021ebbd156a623fef8dfe720d9
devSaifur <saifur2akash@gmail.com>
2024-11-24 01:21:17 +0600

    add animations

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
+import vanjs from 'astro-vanjs'
 
 export default defineConfig({
-  integrations: [tailwind(), , sitemap(), icon()],
+  integrations: [tailwind(), , sitemap(), icon(), vanjs()],
   output: 'server',
   adapter: cloudflare(),
   site: 'https://saifur.pages.dev',
