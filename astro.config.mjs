27cccf06993b1d1f7c278631a775ccb61581b32b
devSaifur <saifur2akash@gmail.com>
2024-11-26 00:05:27 +0600

    improve styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import cloudflare from '@astrojs/cloudflare'
 import sitemap from '@astrojs/sitemap'
+import solid from '@astrojs/solid-js'
 import tailwind from '@astrojs/tailwind'
+import vanjs from 'astro-vanjs'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  integrations: [tailwind(), , sitemap(), icon()],
-  output: 'server',
+  integrations: [tailwind(), , sitemap(), icon(), solid(), vanjs()],
+  output: 'hybrid',
   adapter: cloudflare(),
   site: 'https://saifur.pages.dev',
 })
