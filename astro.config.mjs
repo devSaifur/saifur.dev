48eeb6dd004dbe24cf714ea385f5f67558c01d9d
devSaifur <saifur2akash@gmail.com>
2024-11-26 19:00:29 +0600

    move to next.js setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@ import cloudflare from '@astrojs/cloudflare'
 import sitemap from '@astrojs/sitemap'
 import solid from '@astrojs/solid-js'
 import tailwind from '@astrojs/tailwind'
-import vanjs from 'astro-vanjs'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  integrations: [tailwind(), , sitemap(), icon(), solid(), vanjs()],
+  integrations: [tailwind(), sitemap(), icon(), solid()],
   output: 'hybrid',
   adapter: cloudflare(),
   site: 'https://saifur.pages.dev',
