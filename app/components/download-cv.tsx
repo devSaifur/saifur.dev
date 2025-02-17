7977648e7b9721603b23e825c67e1e20d0fd45b9
devSaifur <saifur2akash@gmail.com>
2025-02-17 21:57:56 +0600

    update cv

--- app/components/download-cv.tsx
+++ app/components/download-cv.tsx
@@ -2,7 +2,7 @@
 
 export function openCV() {
   window.open(
-    'https://cdn.jsdelivr.net/gh/devSaifur/saifur.dev@main/public/assets/SaifurRahman(CV).pdf?row=true',
+    'https://cdn.jsdelivr.net/gh/devSaifur/saifur.dev@main/app/assets/SaifurRahman(CV).pdf?row=true',
     '_blank',
     'noopener,noreferrer'
   )
